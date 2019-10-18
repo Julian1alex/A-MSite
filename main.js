dbafb79b7f92121de9f6a7a66855e67650052780
Mitchell Rhoads <mitchelldrhoads@gmail.com>
2019-10-17 18:21:32 -0700

    redo

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 $(document).ready(function (){
     $.get("https://obscure-tundra-54269.herokuapp.com/fine-dining")
+    .done(function(data)){
+        let apps = data.appetizers
+        let entrees = data.entrees
+        let zerts = data.desserts
+
+        var html = 
+        ``
+    }
 
 
 })
